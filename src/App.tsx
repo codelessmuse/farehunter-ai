fa86e40c13863da29995e2d5d5ea0ea630892ce6
codelessmuse <codelessmuse@gmail.com>
2026-05-30 18:28:22 +0100

    Build React Vite foundation scaffold

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
+import DashboardPage from './pages/DashboardPage';
 import LandingPage from './pages/LandingPage';
-import SearchPage from './pages/SearchPage';
 import ResultsPage from './pages/ResultsPage';
 import SavedSearchesPage from './pages/SavedSearchesPage';
-import DashboardPage from './pages/DashboardPage';
+import SearchPage from './pages/SearchPage';
 
-export default function App(){
- return (<BrowserRouter><Routes><Route element={<Layout/>}><Route path='/' element={<LandingPage/>}/><Route path='/search' element={<SearchPage/>}/><Route path='/results' element={<ResultsPage/>}/><Route path='/saved-searches' element={<SavedSearchesPage/>}/><Route path='/dashboard' element={<DashboardPage/>}/></Route></Routes></BrowserRouter>)
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/results" element={<ResultsPage />} />
+          <Route path="/saved-searches" element={<SavedSearchesPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
